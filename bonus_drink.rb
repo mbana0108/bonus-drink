c4fb85abb861793d983951c326cb42216b688960
YukiMiyazaki <1837yuuki@gmail.com>
2024-08-15 18:14:27 +0900

    Update bonus_drink.rb

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,35 +1,34 @@
-def main
-    bonus = 0
-    sum_drink = 0
-    buy_drink = 0
-  
-    loop do
-      # ユーザーに購入する飲み物の本数を入力してもらう
-      print "How many drinks?\n"
-      num = gets.to_i
-  
-      # 合計の購入数を更新
-      sum_drink += num
-  
-      # 空き瓶によるボーナスを計算
-      bonus = sum_drink / 3
-  
-      # 飲める合計の本数を計算
-      buy_drink = num + bonus
-  
-      puts "Total drinks #{buy_drink}\n\n"
-  
-      # プログラムを続けるかどうかをユーザーに尋ねる
-      print "continue? y/n\n"
-      state = gets.chomp
-  
-      # "n"が入力されたらループを終了
-      break if state == "n"
-    end
-  
-    # 最終的なトータルの飲み物の本数を表示
-    puts "Total drinks #{buy_drink}"
+def calculate_total_drinks(initial_drinks)
+  total_drinks = initial_drinks
+  empty_bottles = initial_drinks
+
+  while empty_bottles >= 3
+    new_drinks = empty_bottles / 3
+    total_drinks += new_drinks
+    empty_bottles = new_drinks + (empty_bottles % 3)
   end
-  
-  main
-  
+
+  total_drinks
+end
+
+def main
+  loop do
+    # ユーザーに購入する飲み物の本数を入力してもらう
+    print "How many drinks? "
+    num = gets.to_i
+
+    # 飲める合計の本数を計算
+    total_drinks = calculate_total_drinks(num)
+
+    puts "Total drinks you can drink: #{total_drinks}\n\n"
+
+    # プログラムを続けるかどうかをユーザーに尋ねる
+    print "Continue? (y/n): "
+    state = gets.chomp.downcase
+
+    # "n"が入力されたらループを終了
+    break if state == "n"
+  end
+end
+
+main
