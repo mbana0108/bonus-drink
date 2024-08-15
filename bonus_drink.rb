9f5ecf4a299847234d54d60871cbd283062768f9
YukiMiyazaki <1837yuuki@gmail.com>
2024-08-15 18:25:34 +0900

    Update bonus_drink.rb

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,25 +1,27 @@
-def calculate_total_drinks(initial_drinks)
-  total_drinks = initial_drinks
-  empty_bottles = initial_drinks
+class BonusDrink
+  def self.total_count_for(initial_drinks)
+    total_drinks = initial_drinks
+    empty_bottles = initial_drinks
 
-  while empty_bottles >= 3
-    new_drinks = empty_bottles / 3
-    total_drinks += new_drinks
-    empty_bottles = new_drinks + (empty_bottles % 3)
+    while empty_bottles >= 3
+      new_drinks = empty_bottles / 3
+      total_drinks += new_drinks
+      empty_bottles = new_drinks + (empty_bottles % 3)
+    end
+
+    total_drinks
   end
-
-  total_drinks
 end
 
 def main
-    # ユーザーに購入する飲み物の本数を入力してもらう
-    print "How many drinks? "
-    num = gets.to_i
+  # ユーザーに購入する飲み物の本数を入力してもらう
+  print "How many drinks? "
+  num = gets.to_i
 
-    # 飲める合計の本数を計算
-    total_drinks = calculate_total_drinks(num)
+  # 飲める合計の本数を計算
+  total_drinks = BonusDrink.total_count_for(num)
 
-    puts "Total drinks you can drink: #{total_drinks}\n\n"
+  puts "Total drinks you can drink: #{total_drinks}\n\n"
 end
 
-main
+main if __FILE__ == $0
